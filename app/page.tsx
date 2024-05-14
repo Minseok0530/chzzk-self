0bfb0a5303b2095817107f52eb17b05e2a5f0cdf
Minseok0530 <mskim@m-solution.page>
2024-05-14 11:04:14 +0900

    더보기 기능 추가

--- app/page.tsx
+++ app/page.tsx
@@ -2,79 +2,74 @@
 
 import ReactPlayer from 'react-player';
 import { useEffect, useRef, useState } from 'react';
-import { createClient } from '@supabase/supabase-js';
+import supabase from '../api/supabase';
 import { Tables } from '../supabase';
-import { BlobOptions } from 'buffer';
-async function onClick(inputText: string) {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-  if (!supabaseUrl || !supabaseKey) {
-    console.log('url or key is not found');
-    return;
-  }
-  const supabase = createClient(supabaseUrl, supabaseKey);
+import nextAppLoader from 'next/dist/build/webpack/loaders/next-app-loader';
 
-  const { data: value } = await supabase
+const index = ['1번 제목', '2번 제목', '3번 제목'];
+// async function getAllLink() {
+
+//   const { data: value } = await supabase
+//     .from('todos')
+//     .select('*')
+//     .returns<Tables<'todos'>[]>();
+//   console.log(
+//     value?.map((o) => {
+//       return o.link;
+//     })
+//   );
+// }
+
+async function getLink(maxnumber: number) {
+  const linkdata = await supabase
     .from('todos')
     .select('*')
+    .limit(maxnumber)
     .returns<Tables<'todos'>[]>();
-  console.log(
-    value?.map((o) => {
-      return o.link;
-    })
-  );
+  const sortLink = linkdata.data?.map((o) => {
+    if (!o) return '';
+    else return o.link;
+  });
+
+  if (sortLink) return sortLink;
 }
 
 export default function Home() {
-  const index = [
-    '1번 제목',
-    '2번 제목',
-    '3번 제목',
-    '4번 제목',
-    '5번 제목',
-    '6번 제목',
-    '7번 제목',
-    '8번 제목',
-    '9번 제목',
-    '10번 제목',
-    '11번 제목',
-    '12번 제목',
-    '13번 제목',
-    '14번 제목',
-    '15번 제목',
-  ];
+  //let data: (string | null)[];
+
+  const [data, setData] = useState<(string | null)[]>([]);
+  const [maxSize, setMaxSize] = useState(5);
+  const [currentIndex, setIndex] = useState(5);
+  const videoList = getLink(maxSize);
+
+  useEffect(() => {
+    const loadData = async () => {
+      const videoList = await getLink(maxSize);
+      if (videoList) {
+        setData(videoList);
+      }
+    };
+    loadData();
+  }, [maxSize]);
+
+  // videoList.then((items) => {
+  //   if (items)
+  //     return (data = items?.map((o) => {
+  //       if (!o) return '';
+  //       return o;
+  //     }));
+  // });
+
+  async function loadMore() {
+    if (maxSize > 5) setMaxSize(5);
+    else setMaxSize(10);
+    console.log(maxSize);
+  }
 
   const [isWindow, setIsWindow] = useState<boolean>(false);
   useEffect(() => {
     setIsWindow(true);
   }, []);
-
-  // const loading = useRef<boolean>(false);
-  // const [page, setPage] = useState({
-  //   current: 1,
-  //   total: 0,
-  // });
-  // const [link, setLinkIndex] = useState([]);
-  // async function getCount() {
-  //   const count = index;
-  //   setPage({
-  //     ...page,
-  //     total: count.length,
-  //   });
-  // }
-  // async function getList(count: number) {
-  //   loading.current = true;
-  // }
-  // useEffect(() => {
-  //   if (!loading.current) {
-  //     const totalCount = getCount();
-  //   }
-  // }, [page]);
-  // function setPageInfo(currentPage: number) {
-  //   setPage({ ...page, current: currentPage });
-  // }
-
-  // const moreview = () => {};
 
   return (
     <div className="flex">
@@ -97,28 +92,32 @@ export default function Home() {
           <div>이 라이브 어떤가요</div>
           <div className="flex">
             {isWindow && (
-              <div className="grid grid-cols-5">
-                {index.map((o, i) => {
-                  return (
-                    <div className="mx-2 p-0" key={i}>
-                      <ReactPlayer
-                        url={'https://www.youtube.com/watch?v=EngW7tLk6R8'}
-                        width="22rem"
-                        height="15rem"
-                        playing={true}
-                        muted={true}
-                        controls={false}
-                        onRewind={true}
-                      />
-                      <div>{o}</div>
-                    </div>
-                  );
-                })}
+              <div>
+                <div className="grid grid-cols-5">
+                  {data?.map((o, i) => {
+                    return (
+                      <div className="mx-2 p-0" key={i}>
+                        <ReactPlayer
+                          url={o === null ? '' : o}
+                          width="22rem"
+                          height="15rem"
+                          playing={true}
+                          muted={true}
+                          controls={false}
+                          onRewind={true}
+                        />
+                        <div>{o}</div>
+                      </div>
+                    );
+                  })}
+                </div>
+                <div>
+                  <button onClick={loadMore}>
+                    {maxSize > 5 ? '접기' : '더보기'}
+                  </button>
+                </div>
               </div>
             )}
-          </div>
-          <div>
-            <button>더보기</button>
           </div>
         </div>
         <div>좋아하실 꺼 같아요</div>
