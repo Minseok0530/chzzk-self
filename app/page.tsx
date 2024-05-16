742cbd1cf76d188200901a8c4b0146ebe607da7d
Minseok0530 <mskim@m-solution.page>
2024-05-16 11:56:41 +0900

    ➕ : 카테고리 기능 추가

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,14 @@ import { MouseEventHandler, useEffect, useRef, useState } from 'react';
 import supabase from '../api/supabase';
 import { Tables } from '../supabase';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+
+import Image, { StaticImageData } from 'next/image';
+
+import eldenring from '../public/category/Eldenring.jpg';
+import justchat from '../public/category/justchat.png';
+import lol from '../public/category/lol.jpg';
+import mhw from '../public/category/Monster_hunter.jpg';
+import vargame from '../public/category/varioust_game.png';
 
 const index = ['1번 제목', '2번 제목', '3번 제목'];
 // async function getAllLink() {
@@ -37,11 +44,44 @@ async function getLink(maxnumber: number) {
   if (sortLink) return sortLink;
 }
 
+async function category() {
+  const category = await supabase
+    .from('category')
+    .select('*')
+    .returns<Tables<'category'>[]>();
+
+  console.log(category);
+
+  if (category) {
+    return category.data?.map((o) => {
+      return {
+        category_name: o.category_name,
+        category_id: o.category_id,
+      };
+    });
+  }
+}
+
 export default function Home() {
   const [data, setData] = useState<
     ({ link: string | null; name: string | null } | undefined)[]
   >([]);
   const [maxSize, setMaxSize] = useState(5);
+  const [category_data, setCategory] = useState<
+    {
+      category_name: string | null;
+      category_id: number | null;
+    }[]
+  >([]);
+
+  useEffect(() => {
+    const loadCategory = async () => {
+      const category_items = await category();
+      console.log(category_items);
+      if (category_items) setCategory(category_items);
+    };
+    loadCategory();
+  }, []);
 
   useEffect(() => {
     const loadData = async () => {
@@ -74,10 +114,12 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex">
+    <div className="flex mr-4 ml-8">
       <div className="flex flex-col w-full">
-        <div className="bg-gray-600 h-20 rounded-xl">
-          <div className="bg-gray-600 hover:bg-white h-20 w-[20%]"></div>
+        <div className="bg-[#1E2022] h-20 rounded-xl mb-6">
+          <div className=" hover:bg-[#2E3033] h-20 w-[20%] rounded-s-xl text-center">
+            광?고
+          </div>
         </div>
         <div>
           {isWindow && (
@@ -99,7 +141,7 @@ export default function Home() {
           )}
         </div>
         <div>
-          <div>이 라이브 어떤가요</div>
+          <p className="text-[1.5rem] font-bold tracking">이 라이브 어때요?</p>
           <div className="flex">
             {isWindow && (
               <div>
@@ -113,7 +155,6 @@ export default function Home() {
                           query: { url: o?.link },
                         }}
                       >
-                        {' '}
                         <button
                           className="flex flex-col justify-center-center hover:bg-gray-800"
                           //onClick={() => pageVideoMove(o?.link ?? '')}
@@ -136,16 +177,45 @@ export default function Home() {
                     );
                   })}
                 </div>
-                <div>
-                  <button onClick={loadMore}>
-                    {maxSize > 5 ? '접기' : '더보기'}
+                <div className="flex justify-center mt-3 items-center">
+                  <div className="w-full border-t border-gray-300" />
+                  <button
+                    onClick={loadMore}
+                    className="border rounded-full w-52 flex justify-center"
+                  >
+                    <div className="flex">
+                      {maxSize > 5 ? '접기' : '더보기'}
+                      <span className="material-symbols-outlined">
+                        keyboard_arrow_down
+                      </span>
+                    </div>
                   </button>
+                  <div className="w-full border-t border-gray-300" />
                 </div>
               </div>
             )}
           </div>
         </div>
-        <div>좋아하실 꺼 같아요</div>
+        <div className="">
+          좋아하실 꺼 같아요
+          <div className="flex">
+            {category_data.map((o, i) => {
+              return (
+                <div key={i}>
+                  <button>
+                    <Image
+                      className=""
+                      src={`/category/${o.category_id}.png`}
+                      alt={''}
+                      width={150}
+                      height={150}
+                    />
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
         <div>신입 스트리머 인사드립니다</div>
       </div>
     </div>
