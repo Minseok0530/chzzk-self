f666d5bd4e289a7e0923476c67bee571fee0b87f
Minseok0530 <mskim@m-solution.page>
2024-05-21 10:23:34 +0900

    ➕ : 메뉴 및 헤더 다시 보이도록 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
           rel='stylesheet'
         />
       </head>
-      {path === '/login' || '/register' ? (
+      {path === ('/login' || '/register') ? (
         <body>
           <div className='w-full flex justify-center h-full bg-white'>
             <div className='w-full'>{children}</div>
