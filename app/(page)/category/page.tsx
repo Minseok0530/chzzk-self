8ddb337343cb07d248c9c64d3131d17f345ef447
Minseok0530 <mskim@m-solution.page>
2024-05-23 15:36:10 +0900

    ➕ : 카테고리 페이지 진행

--- app/(page)/category/page.tsx
+++ app/(page)/category/page.tsx
@@ -1,3 +1,56 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import supabase from '../../../api/supabase';
+import { Tables } from '../../../supabase';
+import Image from 'next/image';
+
 export default function Home() {
-  return <div>Category</div>;
+  const [category_Data, setCategoryData] = useState<
+    {
+      category_id: number | null;
+      category_name: string | null;
+      created_at: string;
+      id: number;
+    }[]
+  >([]);
+
+  async function categoryLoad() {
+    const { data, error } = await supabase
+      .from('category')
+      .select('*')
+      .returns<Tables<'category'>[]>();
+    if (error) {
+      console.error(error);
+      return;
+    }
+    setCategoryData(data);
+  }
+
+  useEffect(() => {
+    categoryLoad();
+  }, []);
+  return (
+    <div>
+      카테고리
+      <div className='grid grid-cols-8'>
+        {category_Data.map((o, i) => {
+          return (
+            <div key={i} className='mr-4 mb-10'>
+              <button>
+                <Image
+                  className='rounded-xl'
+                  src={`/category/${o.category_id}.png`}
+                  alt={''}
+                  width={200}
+                  height={200}
+                />
+                <div className='mt-4 text-start'>{o.category_name}</div>
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
